3a8a7dc44fbbe363a2249524901f1bd906dbdbb1
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-26 21:33:11 +0100

    terminado primer ejercicio

--- 1bejercicio.py
+++ 1bejercicio.py
@@ -2,3 +2,5 @@ import json
 fich=open("/home/franhidalgo/Documentos/LM/Json/asociaciones.txt","r")
 asociaciones=json.load(fich)
 fich.close()
+for a in asociaciones ["directorios"]["directorio"]:
+	print a["nombre"]["content"]
