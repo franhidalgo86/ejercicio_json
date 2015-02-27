b295d84b15c0e3d9dc415f65648a30d9fdefcfac
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 17:46:16 +0100

    Ejercicio 5 modicando

--- 5ejercicio.py
+++ 5ejercicio.py
@@ -2,4 +2,4 @@ import json
 fich=open("/home/franhidalgo/Documentos/LM/Json/asociaciones.txt","r")
 asociaciones=json.load(fich)
 fich.close()
-
+for telf in asociaciones ["directorios"]["directorio"]:
