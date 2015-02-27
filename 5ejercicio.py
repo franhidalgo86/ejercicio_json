82961611db339ba22406b4fbfc54931bf100a5dc
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 17:48:11 +0100

    Ejercicio 5 terminando

--- 5ejercicio.py
+++ 5ejercicio.py
@@ -3,3 +3,5 @@ fich=open("/home/franhidalgo/Documentos/LM/Json/asociaciones.txt","r")
 asociaciones=json.load(fich)
 fich.close()
 for telf in asociaciones ["directorios"]["directorio"]:
+	if " // " in telf["telefono"]["content"]:
+		print telf["nombre"]["content"] 
