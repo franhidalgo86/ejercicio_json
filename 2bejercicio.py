203e27784948da5d5224efde6dddbbc0e8015c27
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 14:46:04 +0100

    Ejercicio 2 terminando

--- 2bejercicio.py
+++ 2bejercicio.py
@@ -5,3 +5,6 @@ fich.close()
 for a in asociaciones ["directorios"]["directorio"]:
 	print a["nombre"]["content"]
 asociacion=raw_input("Mete una Asociacion: ")
+for pre in asociaciones:
+	if pre["nombre"]["content"]==asociacion:
+		print pre["descripcion"]["content"]
