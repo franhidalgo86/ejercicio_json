5a108d55b73abe65e89305922d28694a916fc6af
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 17:31:44 +0100

    Ejercicio 4 creando

--- 4ejercicio.py
+++ 4ejercicio.py
@@ -2,4 +2,6 @@ import json
 fich=open("/home/franhidalgo/Documentos/LM/Json/asociaciones.txt","r")
 asociaciones=json.load(fich)
 fich.close()
-
+contador=0
+for corre in asociaciones ["directorios"]["directorio"]:
+	if "@" in corre["correo-electronico"]:
