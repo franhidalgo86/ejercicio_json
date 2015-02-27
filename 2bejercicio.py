9ab0d035006a685bac056ccd789f3e7462cdcd54
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 14:41:39 +0100

    Ejercicio 2 modificando

--- 2bejercicio.py
+++ 2bejercicio.py
@@ -2,4 +2,6 @@ import json
 fich=open("/home/franhidalgo/Documentos/LM/Json/asociaciones.txt","r")
 asociaciones=json.load(fich)
 fich.close()
-
+for a in asociaciones ["directorios"]["directorio"]:
+	print a["nombre"]["content"]
+asociacion=raw_input("Mete una Asociacion: ")
